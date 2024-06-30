5f908c5be98ef290876e3bb4cb10f2771f6d6480
Nicolai Syvertsen <saivert@saivert.com>
2024-06-30 22:41:51 +0200

    Update po files.

--- build-aux/make-locale-dir.sh
+++ build-aux/make-locale-dir.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 export BUILD_ROOT="$1"
 
-# English
-mkdir -p $BUILD_ROOT/locale-test/locale/en/LC_MESSAGES
-cp $BUILD_ROOT/po/en/LC_MESSAGES/pwvucontrol.mo $BUILD_ROOT/locale-test/locale/en/LC_MESSAGES
-
-# Norwegian
-mkdir -p $BUILD_ROOT/locale-test/locale/nb/LC_MESSAGES
-cp $BUILD_ROOT/po/nb/LC_MESSAGES/pwvucontrol.mo $BUILD_ROOT/locale-test/locale/nb/LC_MESSAGES
-
+mkdir -p $BUILD_ROOT/locale-test/
+ln -s $BUILD_ROOT/po $BUILD_ROOT/locale-test/locale
