9daf251925517cec85ae98ffa22a530755c317e7
Nicolai Syvertsen <saivert@saivert.com>
2025-03-16 21:26:38 +0100

    Finally fix squiggles for OUT_DIR

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs::copy;
+use std::path::{Path, PathBuf};
+
+fn main() {
+    // This works around issue with rust-analyzer not picking up OUT_DIR environment variable
+    // even if specified via LSP settings (BUG).
+    // Having a build.rs file present activates the OUT_DIR environment variable but we
+    // also need to copy the generated config.rs file there.
+    println!("cargo::rerun-if-changed=src/config.rs.in");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let sourcepath: PathBuf = ["builddir", "src", "config.rs"].iter().collect();
+    if !Path::exists(&sourcepath) {
+        panic!("Please configure the project with meson once to generate config.rs!");
+    }
+    let destpath: PathBuf = [&out_dir, "config.rs"].iter().collect();
+    copy(sourcepath, destpath).expect("Copying config.rs success");
+}
