4bc32fc1dc775315bcaa4bb10ef4663beec100b0
Nicolai Syvertsen <saivert@saivert.com>
2025-03-18 19:57:01 +0100

    Let build.rs fallback to builddir/src.
    So we don't have to specify rust-analyzer env var in most cases.

--- build.rs
+++ build.rs
@@ -9,10 +9,15 @@ fn main() {
     // also need to copy the generated config.rs file there.
     println!("cargo::rerun-if-changed=src/config.rs.in");
     let out_dir = env::var("OUT_DIR").unwrap();
-    let codegen_dir = env::var("CODEGEN_DIR").unwrap();
+    let codegen_dir = env::var("CODEGEN_DIR").unwrap_or("builddir/src".to_owned());
     let sourcepath: PathBuf = [&codegen_dir, "config.rs"].iter().collect();
     if !Path::exists(&sourcepath) {
-        panic!("Please configure the project with meson once to generate config.rs!");
+        panic!(
+            r#"
+            Please configure the project with meson once to generate config.rs!
+            If it is not generated in builddir/src then you need to specify the path with the CODEGEN_DIR environment variable.
+            "#
+        );
     }
     let destpath: PathBuf = [&out_dir, "config.rs"].iter().collect();
     copy(sourcepath, destpath).expect("Copying config.rs success");
